8136fae8757dd9210eced454d1b1f72a5f782f6b
Heliomar P. Marques <heliomarpm@gmail.com>
2022-11-13 19:16:34 -0300

    first commit

---
+++ ClosingEffects/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace ClosingAForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelClose = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelSeizure = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelClose
+            // 
+            this.labelClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelClose.AutoSize = true;
+            this.labelClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.labelClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelClose.Location = new System.Drawing.Point(758, 9);
+            this.labelClose.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.labelClose.Name = "labelClose";
+            this.labelClose.Size = new System.Drawing.Size(27, 25);
+            this.labelClose.TabIndex = 3;
+            this.labelClose.Text = "X";
+            this.labelClose.Click += new System.EventHandler(this.labelClose_Click);
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(309, 407);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(182, 49);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Shrink Middle";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.Label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(309, 330);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(182, 49);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Shrink Center";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Click += new System.EventHandler(this.Label5_Click);
+            // 
+            // labelSeizure
+            // 
+            this.labelSeizure.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelSeizure.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelSeizure.Location = new System.Drawing.Point(309, 640);
+            this.labelSeizure.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.labelSeizure.Name = "labelSeizure";
+            this.labelSeizure.Size = new System.Drawing.Size(182, 49);
+            this.labelSeizure.TabIndex = 10;
+            this.labelSeizure.Text = "Random Lights";
+            this.labelSeizure.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelSeizure.Click += new System.EventHandler(this.LabelSeizure_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(309, 255);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 49);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Shrink Up / Left";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(309, 184);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 49);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Shrink Left";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(309, 112);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 51);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Shrink In";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(800, 800);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelSeizure);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelClose);
+            this.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelClose;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelSeizure;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ClosingEffects/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClosingAForm
+{
+    public partial class Form1 : Form
+    {
+        private readonly int _initialHeight;
+        private readonly int _initialWidth;
+
+        private Timer _timer = new Timer();
+        private Random _random = new Random();
+        private int _style = 0;
+        private int _lights = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _initialHeight = this.Height;
+            _initialWidth = this.Width;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BackColor = Color.FromArgb(_random.Next(0, 255), _random.Next(0, 255), _random.Next(0, 255));
+            _timer.Tick += new EventHandler(Resize_Tick);
+        }
+
+
+        void StartTimer(int i)
+        {
+            _style = i;
+            BackColor = Color.FromArgb(_random.Next(0, 255), _random.Next(0, 255), _random.Next(0, 255));
+            _timer.Interval = 1;
+            _timer.Start();
+        }
+
+
+        private void Resize_Tick(object sender, EventArgs e)
+        {
+            int reduce = 10;
+            int limit = reduce + 2;
+
+            switch (_style)
+            {
+                case 0:
+                    _lights++;
+                    if (_lights <= 500)
+                    {
+                        BackColor = Color.FromArgb(_random.Next(0, 255), _random.Next(0, 255), _random.Next(0, 255));
+                    }
+                    else
+                    {
+                        WindowState = FormWindowState.Normal;
+                        ToggleLabels();
+                        ReDraw();
+                    }
+                    break;
+
+                case 1:
+                    if ((Size.Width >= limit) || (Size.Height >= limit))
+                    {
+                        Size = new Size(Width - reduce, Height = Width);
+                        Opacity -= .002;
+                        CenterToScreen();
+                    }
+                    else
+                    {
+                        ReDraw();
+                    }
+                    break;
+
+                case 2:
+                    if (Size.Width >= limit)
+                    {
+                        Size = new Size(Width - reduce, Height);
+                        Opacity -= .002;
+                    }
+                    else
+                    {
+                        ReDraw();
+                    }
+                    break;
+
+                case 3:
+                    if ((Size.Width >= limit) || (Size.Height >= limit))
+                    {
+                        Size = new Size(Width - reduce, Height = Width);
+                        Opacity -= .002;
+                    }
+                    else
+                    {
+                        ReDraw();
+                    }
+                    break;
+
+                case 5:
+                    if (Size.Width >= limit)
+                    {
+                        Size = new Size(Width - reduce, Height);
+                        Opacity -= .002;
+                        CenterToScreen();
+                    }
+                    else
+                    {
+                        ReDraw();
+                    }
+                    break;
+
+                case 6:
+                    if (Size.Height >= limit)
+                    {
+                        Size = new Size(Width, Height - reduce);
+                        Opacity -= .002;
+                        CenterToScreen();
+                    }
+                    else
+                    {
+                        ReDraw();
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+
+        void ReDraw()
+        {
+            _timer.Stop();
+            Opacity = 1;
+            Size = new Size(_initialWidth, _initialHeight);
+            CenterToScreen();
+        }
+
+        private void LabelSeizure_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Maximized;
+            ToggleLabels();
+            _lights = 0;
+            StartTimer(0);
+        }
+
+        private void ToggleLabels()
+        {
+            if (WindowState == FormWindowState.Maximized)
+            {
+                foreach (var label in Controls.OfType<Label>())
+                {
+                    label.Hide();
+                }
+            }
+            else
+            {
+                foreach (var label in Controls.OfType<Label>())
+                {
+                    label.Show();
+                }
+            }
+        }
+
+        private void labelSeizure_Click_1(object sender, EventArgs e)
+        {
+            StartTimer(6);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            StartTimer(1);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            StartTimer(2);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            StartTimer(3);
+        }
+
+        private void Label5_Click(object sender, EventArgs e)
+        {
+            StartTimer(5);
+        }
+
+        private void Label6_Click(object sender, EventArgs e)
+        {
+            StartTimer(6);
+        }
+
+        private void labelClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
